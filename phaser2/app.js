b5816900e80168b852ef51dc16ba7db64ccf9cbc
Raycelle <raycelle@firstcodeacademy.com>
2017-06-02 00:36:31 +0800

    phaser

--- phaser2/app.js
+++ phaser2/app.js
@@ -7,7 +7,7 @@ var life = 3;
 function preload(){
   game.load.image('sky', 'assets/sky.png');
   game.load.image('ground', 'assets/platform.png');
-  game.load.image('platform','assets/platform2');
+  game.load.image('platform','assets/platform2.png');
   game.load.image('star', 'assets/star.png');
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
   game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
