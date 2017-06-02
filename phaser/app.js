4574695cc32cafa12eeaf9b94c3b352a878ff7f1
Raycelle <raycelle@firstcodeacademy.com>
2017-06-03 00:23:11 +0800

    phaser

--- phaser/app.js
+++ phaser/app.js
@@ -10,7 +10,7 @@ function preload(){
   game.load.image('star', 'assets/star.png');
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
   game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
-  game.load.image('health','assets/firstaid.png')
+  game.load.image('health','assets/firstaid.png');
 }
 
 function create(){
