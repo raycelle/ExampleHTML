6ba4b9d0b24ec6ed079e6736c4c900cb997522d0
Raycelle <raycelle@firstcodeacademy.com>
2017-06-10 09:46:23 +0800

    ...

--- phaserTest/app.js
+++ phaserTest/app.js
@@ -2,7 +2,7 @@ console.log("test")
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload:preload, create:create, update:update});
 var score = 0;
-var life = 3;
+var life = 5;
 
 function preload(){
   game.load.image('sky', 'assets/sky.png');
